020a398c2a12fce44c8d5ac9e56f5b4ff035cbe9
idohalevi <ido.h@logz.io>
2018-07-04 11:18:31 +0300

    using foreach for Java7 support

--- src/main/java/jenkins/plugins/logstash/persistence/LogzioDao.java
+++ src/main/java/jenkins/plugins/logstash/persistence/LogzioDao.java
@@ -86,9 +86,12 @@ public class LogzioDao extends AbstractLogstashIndexerDao {
         JsonObject logLine = new JsonObject();
         logLine.addProperty("message", logMsg);
         logLine.addProperty("@timestamp", LogstashConfiguration.getInstance().getDateFormatter().format(Calendar.getInstance().getTime()));
-        jsonData.keySet().stream()
-        .filter(key -> !(key.equals("message")))
-        .forEach(key -> logLine.addProperty(key.toString(), jsonData.getString(key.toString())));
+        for(Object key : jsonData.keySet()){
+            String keyStr = (String) key;
+            if (!keyStr.equals("message")){
+                logLine.addProperty(key.toString(), jsonData.getString(key.toString()));
+            }
+        }
         return logLine;
     }
 
